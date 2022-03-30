19c4f75d61c6eeae6178870b0e29d09d7af03ad8
AKITO-PC\akito <akitosun0614@gmail.com>
2022-03-30 15:53:24 +0800

    加入專案檔案。

---
+++ WorkDoAuto/WorkDoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkDoAuto
+{
+    public class WorkDoService
+    {
+        public void PunchIn()
+        {
+
+        }
+        public void PunchOut()
+        {
+
+        }
+    }
+}
